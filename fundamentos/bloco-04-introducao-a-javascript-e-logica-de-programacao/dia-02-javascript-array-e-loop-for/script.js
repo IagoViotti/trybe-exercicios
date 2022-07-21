82b4c84f9cf4de7079188f31c9f51a9ecfe76281
Iago Viotti <iago.viotti@gmail.com>
2022-07-21 15:29:46 -0300

    exercicios do conteudo de array

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,23 @@
+//exercício 1 -- array
+console.log('--exercicio 1--')
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//exercício 2 -- array
+console.log('--exercicio 2--')
+
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//exercicio 3 -- array
+console.log('--exercicio 3--')
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push('Contato')
+
+console.log(menu3);
