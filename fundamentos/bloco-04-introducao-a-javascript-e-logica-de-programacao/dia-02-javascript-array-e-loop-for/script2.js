9f40150563dc231ba992e108862738009ba89918
Iago Viotti <iago.viotti@gmail.com>
2022-07-22 10:47:09 -0300

    exercios 4.2 completos

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script2.js
@@ -63,4 +63,53 @@ for (let indice = 0; indice < numbers.length; indice += 1) {
             maiorValor = numbers[indice]
         }
 }
-    console.log(maiorValor)
+console.log(maiorValor)
+
+//exercicio 6
+console.log('--exercicio 6--')
+
+let menorValor = numbers[0];
+
+for (let indice = 0; indice < numbers.length; indice += 1) {     
+        if (numbers[indice] < menorValor) {
+            menorValor = numbers[indice]
+        }
+}
+console.log(menorValor)
+
+//exercicio 7
+console.log('--exercicio 7--')
+
+let valoresImpares = 0;
+
+for (let aux = 0; aux < numbers.length; aux += 1) {
+    if (numbers[aux] % 2 !== 0) {
+        valoresImpares += 1
+    }
+}
+    if (valoresImpares > 0) {
+        console.log(valoresImpares)
+    }
+    else {
+        console.log('nenhum valor ímpar encontrado')
+    }
+
+//exercicio 8
+console.log('--exercicio 8--')
+
+let arrayNumero = [];
+
+for (let index = 1; index <= 25; index += 1) {
+    arrayNumero.push(index)
+}
+
+console.log(arrayNumero)
+
+//exercicio 9
+console.log('--exercicio 9--')
+
+for (let index = 0; index < arrayNumero.length; index += 1) {
+    let divisaoPorDois = arrayNumero[index] / 2;
+    console.log(divisaoPorDois)
+}
+
