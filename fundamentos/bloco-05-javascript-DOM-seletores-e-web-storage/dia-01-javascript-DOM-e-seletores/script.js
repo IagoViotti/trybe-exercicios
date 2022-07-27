a622f4cd2e2c499d515c5ae260670f6f743edc4f
Iago Viotti <iago.viotti@gmail.com>
2022-07-27 19:44:08 -0300

    conteúdo de fixação dos exercicios/5.1

---
+++ fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-01-javascript-DOM-e-seletores/script.js
@@ -0,0 +1,13 @@
+document.getElementById('header-container').style.backgroundColor = 'rgb(0,176,105)'
+
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = 'rgb(255,159,132)'
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = 'rgb(165,0,243)'
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = 'rgb(165,0,243)'
+
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = 'rgb(249,219,94)'
+
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = 'rgb(35,37,37)'
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = 'rgb(35,37,37)'
+
+document.getElementsByTagName('footer')[0].style.backgroundColor = 'rgb(0,53,51)'
