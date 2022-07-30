908015c2fab829cd6902154ee105092f47d2d869
Iago Viotti <iago.viotti@gmail.com>
2022-07-30 01:00:11 -0300

    exercicios contuedo terminados/5.3

--- fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-03-javascript-eventos/main.js
@@ -25,14 +25,25 @@ thirdLi.addEventListener('click', addClassTech);
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
-
+input.addEventListener('input', function(event) {
+  const techFinder = document.querySelector('.tech');
+  techFinder.innerText = event.target.value;
+});
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
 
+myWebpage.addEventListener('dblclick',function() {
+  window.location.replace('https://iagoviotti.github.io/')
+})
+
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+
+myWebpage.addEventListener('mouseover', function() {
+  myWebpage.style.color = '#2fc18c'
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
