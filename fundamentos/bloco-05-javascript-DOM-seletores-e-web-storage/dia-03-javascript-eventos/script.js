4fc197fc7d68010ad6e69a3f5c28529483cc6e2c
Iago Viotti <iago.viotti@gmail.com>
2022-07-30 14:43:48 -0300

    exercicio 1 completo/5.3

--- fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-03-javascript-eventos/script.js
+++ fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-03-javascript-eventos/script.js
@@ -13,4 +13,31 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  // Escreva seu código abaixo
+function createDaysOfTheMonth() {
+  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDaysList = document.getElementById('days');
+
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+      const dia = decemberDaysList[index];
+      const diaListElement = document.createElement('li');
+      diaListElement.innerHTML = dia;
+      if (diaListElement.innerText == 24 || diaListElement.innerText == 25 || diaListElement.innerText == 31) {
+        diaListElement.className = 'day holiday';
+      }
+      else {
+        diaListElement.className = 'day'
+      }
+      monthDaysList.appendChild(diaListElement);
+    };
+    
+    for (index = 5; index < decemberDaysList.length; index += 7) {
+      const sextasDeDezembro = document.querySelectorAll('#days li')[index]
+      sextasDeDezembro.classList.add('friday')
+
+      console.log(sextasDeDezembro)
+    };
+    
+  };
+
+createDaysOfTheMonth()
