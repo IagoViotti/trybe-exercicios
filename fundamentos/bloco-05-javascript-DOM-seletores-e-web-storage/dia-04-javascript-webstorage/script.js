226c8b947f68b88ef99e552aac9c318ba932eef8
Iago Viotti <iago.viotti@gmail.com>
2022-08-01 19:29:59 -0300

    requisito 1 completo/5.4

--- fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-04-javascript-webstorage/script.js
+++ fundamentos/bloco-05-javascript-DOM-seletores-e-web-storage/dia-04-javascript-webstorage/script.js
@@ -1,5 +1,7 @@
-const inputText = document.getElementById('input-texto');
+const inputText = document.querySelector('#input-background');
 
 function corDeFundo() {
-
+    document.body.style.backgroundColor = inputText.value;
 }
+
+inputText.addEventListener('change',corDeFundo)
