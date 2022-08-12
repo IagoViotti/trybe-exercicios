b3226ebbd82bb0f43f1d0d1f576b41e72d80985a
Iago Viotti <iago.viotti@gmail.com>
2022-08-12 19:17:21 -0300

    exercicio parte 1 realizado/7.1

---
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,19 @@
+//exercicio 1
+const testingScope = escopo => {
+    if (escopo === true) {
+      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
+
+//exercicio 2 
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = array => array.sort((a,b)=>a-b)
+
+console.log(`Os números ${sortOddsAndEvens(oddsAndEvens)} se encontram ordenados de forma crescente!`)
