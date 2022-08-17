fc1c5f42b37331108e46f0aabeaf0b2792fea7e7
Iago Viotti <iago.viotti@gmail.com>
2022-08-17 10:45:13 -0300

    exercicios 1 completo/7.3

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.js
@@ -1,3 +1,8 @@
-
+const sum = ( num1, num2) => { 
+  if (typeof num1 === 'string' || typeof num2 === 'string') {
+    throw new Error('parameters must be numbers')
+  }
+  return num1 + num2;
+}
 
 module.exports = sum
--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,3 +1,13 @@
 const sum = require('./sum.js')
 
-describe() 
+test('soma 4 + 5 e retorna 9', () => {
+  expect(sum( 4, 5)).toBe(9)
+});
+
+test('soma 0 + 0 e retorna 0', () => {
+  expect(sum( 0, 0)).toBe(0)
+});
+
+test(`lança erro quando recebe parâmetros 4 e "5"`, () => {
+  expect(() => sum(4, "5")).toThrow('parameters must be numbers')
+})
