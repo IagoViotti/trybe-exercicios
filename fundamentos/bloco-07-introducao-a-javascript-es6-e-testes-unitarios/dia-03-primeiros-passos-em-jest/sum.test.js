104f7675c49ef820c648a4b5599f7224bc1a0f90
Iago Viotti <iago.viotti@gmail.com>
2022-08-19 18:36:24 -0300

    exercicio 2 não completo/7.3

--- fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -1,13 +1,17 @@
-const sum = require('./sum.js')
+const {sum, myRemove} = require('./sum.js')
 
-test('soma 4 + 5 e retorna 9', () => {
-  expect(sum( 4, 5)).toBe(9)
+describe('Testes da função sum', () => {
+  it('soma 4 + 5 e retorna 9', () => {
+    expect(sum( 4, 5)).toBe(9)
+  });
+  
+  it('soma 0 + 0 e retorna 0', () => {
+    expect(sum( 0, 0)).toBe(0)
+  });
+  
+  it(`lança erro quando recebe parâmetros 4 e "5"`, () => {
+    expect(() => sum(4, "5")).toThrow('parameters must be numbers')
+  });
+  
 });
 
-test('soma 0 + 0 e retorna 0', () => {
-  expect(sum( 0, 0)).toBe(0)
-});
-
-test(`lança erro quando recebe parâmetros 4 e "5"`, () => {
-  expect(() => sum(4, "5")).toThrow('parameters must be numbers')
-})
