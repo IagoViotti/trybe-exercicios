ed218f15369cd3f7bf94c2ced4a7ad9bae8f03f5
Iago Viotti <iago.viotti@gmail.com>
2023-08-17 18:26:17 -0300

    conteúdo python 2.4

---
+++ python/secao-02-programacao-orientada-a-objetos/dia-04-classes-abstratas-interfaces-protocolos/protocols_example/databases/postgresql.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class PostgresConnection:
+    def execute(self, query: str) -> List[str]:
+        return [
+            f"Postgres executou a query `{query}`",
+            "e retornou mais uma string adicional",
+        ]
+
+
+class PostgresDatabase:
+    def connect(self, connection_url: str = "123") -> PostgresConnection:
+        if connection_url == "123":
+            raise ValueError("Invalid connection")
+        return PostgresConnection()
