ace90e556250c718de43f781972b9c6b06a24da8
Iago Viotti <iago.viotti@gmail.com>
2023-08-28 15:53:24 -0300

    conteudo python 3.3

---
+++ python/secao-03-raspagem-de-dados/dia-03-mongodb-e-crawlers/insert_many.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.catalogue
+# cada book representa um dado obtido na raspagem
+documents = [
+    {
+        "title": "A Light in the Attic",
+    },
+    {
+        "title": "Tipping the Velvet",
+    },
+    {
+        "title": "Soumission",
+    },
+]
+db.books.insert_many(documents)
+client.close()  # fecha a conexão com o banco de dados
