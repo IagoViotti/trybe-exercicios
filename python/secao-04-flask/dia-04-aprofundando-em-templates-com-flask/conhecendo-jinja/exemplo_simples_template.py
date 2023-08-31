b8535cd6885e33edf6fb75fe1b2bba96aa9b8597
Iago Viotti <iago.viotti@gmail.com>
2023-08-31 20:36:00 -0300

    conteúdo python 4.4

---
+++ python/secao-04-flask/dia-04-aprofundando-em-templates-com-flask/conhecendo-jinja/exemplo_simples_template.py
@@ -0,0 +1,11 @@
+from jinja2 import Template
+
+# Carrega um template a partir de uma string
+template_string = "Eu sou um {{ nome }}"
+template = Template(template_string)
+
+# Renderiza o template com um contexto específico
+output = template.render(nome='template')
+
+# Impime o resultado
+print(output)
